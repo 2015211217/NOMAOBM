bc5efcd0578647097e0fcbd62a9418d012f2a54a
JiangXuanke <15223370366@163.com>
2023-05-18 16:40:41 +0900

    5.18

--- Baseline_SDA.py
+++ Baseline_SDA.py
@@ -6,19 +6,19 @@ def Baseline_SDA(runs, number_of_PRB, max_perPRB, power_matrix, number_of_slots_
     connected_devices = 0
     #The version of code for the journal paper only works for each subcarrier only have one slot
     for t in range(runs):
-        start_time = datetime.datetime.now()
+        start_time = datetime.datetime.now().timestamp()
         p_list = power_matrix[t]
-        PRB_power_sequence = np.zeros((1, number_of_PRB))
+        PRB_power_sequence = np.zeros(number_of_PRB)
 
         for i in range(number_of_slots_per_PRB * number_of_PRB * number_of_device):
             PRB_i = (i % (number_of_slots_per_PRB * number_of_PRB)) // number_of_slots_per_PRB
-            if (PRB_power_sequence[PRB_i] + p_list[i] < max_perPRB):
+            if ((PRB_power_sequence[PRB_i] + p_list[i]) < max_perPRB):
                 PRB_power_sequence[PRB_i] += p_list[i]
                 for j in range((i // (number_of_slots_per_PRB * number_of_PRB)) * (number_of_slots_per_PRB * number_of_PRB),(i // (number_of_slots_per_PRB * number_of_PRB)+1) * (number_of_slots_per_PRB * number_of_PRB)):
                     p_list[j] = sys.maxsize
                 index_slot = (i % (number_of_slots_per_PRB * number_of_PRB))
                 for j in range(number_of_device):
                     p_list[j * (number_of_slots_per_PRB * number_of_PRB) + index_slot] = sys.maxsize
-                    
-        datetime_runs += datetime.datetime.now() - start_time
+
+        datetime_runs += datetime.datetime.now().timestamp() - start_time
     return connected_devices / runs, datetime_runs / runs
--- MIPSolution.py
+++ MIPSolution.py
@@ -17,21 +17,24 @@ def MIPBranchCutGurobi(runs, power_matrix, pathloss_matrix, number_of_edges, num
         for i in range(number_of_edges):
             obj += X[i]
         MODEL.setObjective(obj, gurobipy.GRB.MAXIMIZE)
+        MODEL.update()
+
         for i in range(number_of_devices):#one device can only connect to one slot
-            MODEL.addConstr(sum(X[i * (number_of_edges/number_of_devices) + j] for j in range(number_of_edges/number_of_devices)) <= 1)
-        for i in range(number_of_edges/number_of_devices):#one slot can only connect to one device
-            MODEL.addConstr(sum(X[j * (number_of_edges/number_of_devices) + i] for j in range(number_of_devices)) <= 1)
+            MODEL.addConstr(sum(X[int(i * (number_of_edges/number_of_devices) + j)] for j in range(int(number_of_edges/number_of_devices))) <= 1)
+        for i in range(int(number_of_edges/number_of_devices)):#one slot can only connect to one device
+            MODEL.addConstr(sum(X[int(j * (number_of_edges/number_of_devices) + i)] for j in range(number_of_devices)) <= 1)
+
         #one subcarrier can only hold L devices, automatically satisfied
         number_slots_per_PRB = number_of_edges / (number_of_devices * number_of_PRBs)
         for i in range(number_of_PRBs):
             MODEL.addConstr(sum(
-                X[k * (number_of_edges/number_of_devices) + i * number_slots_per_PRB + j]
-                for j, k in zip(range(number_slots_per_PRB), range(number_of_devices))) <= power_max)
-
+                X[int(k * (number_of_edges/number_of_devices) + i * number_slots_per_PRB + j)]
+                for j, k in zip(range(int(number_slots_per_PRB)), range(int(number_of_devices)))) <= power_max)
         for i in range(number_of_edges):
-            MODEL.addConstr(X[i] * pathloss_list[i] * (power_list[i] - Xi * sum(power_list[j] * X[j] for j in range(0, i - number_of_edges / number_of_devices)))
+            MODEL.addConstr(X[i] * pathloss_list[i] * (power_list[i] - Xi * sum(power_list[j] * X[j] for j in range(0, int(i - number_of_edges / number_of_devices))))
                             >= Xi * Noise * X[i])
 
+        MODEL.update()
         GetX = MODEL.getAttr('X', X)
         for i in range(number_of_edges):
             connected_devices += GetX[i]
